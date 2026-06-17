99baf01213c965cff437b0eb58cb843d781830b6
Noam Rothschild <noamr.lego@gmail.com>
2026-06-17 15:12:21 +0300

    fix: brought back the fix for sveltejs/svelte/issues/8112

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,9 +5,12 @@
     let { data } = $props();
 </script>
 
+<!-- the if guard fixes https://github.com/sveltejs/svelte/issues/8112 -->
+{#if typeof window !== "undefined"}
 <h1 class="statement">here are some of the things I do on my free time and for assignments...</h1>
 
 <ProjectsInfo></ProjectsInfo>
+{/if}
 
 <style>
     h1 {
